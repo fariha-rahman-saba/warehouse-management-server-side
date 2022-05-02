5dc041ab3648b4083452f8060c1aac9b5e2ddc78
saba rahman <sabarahman.official@gmail.com>
2022-05-02 23:37:25 +0600

    backend created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 
 // middleware
@@ -15,3 +16,33 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('listening to', port);
 });
+
+async function run () {
+    try {
+        await client.connect();
+        console.log("database connected");
+
+        const itemCollection = client.db("bookMania").collection("items");
+        const userItemsCollection = client.db("bookMania").collection("userItems");
+
+        app.post("/addItems", async (req, res) => {
+            const item = req.body;
+
+        });
+
+        app.get('/manage-inventory', async (req, res) => {
+            const items = await itemCollection.find({}).toArray();
+            req.send(items);
+        });
+
+        app.get('/my-items', async (req, res) => {
+            const userItems = await userItemsCollection.find({ email }).toArray();
+            req.send(userItems);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
