777daf3f332f4511be44effeb9022944e9e66f22
saba rahman <sabarahman.official@gmail.com>
2022-05-05 10:31:44 +0600

    database working

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 4000;
 
-
+const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('hello world');
@@ -19,8 +19,23 @@ app.listen(port, () => {
     console.log('listening to', port);
 });
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mjof2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER2}:${process.env.DB_PASSWORD2}@cluster0.jewnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// async function run () {
+//     try {
+//         await client.connect();
+//         const itemCollection = client.db('bookMania').collection('items');
+//         console.log("database connected");
+//     }
+//     finally {
+
+//     }
+// }
+
+// run().catch(console.dir);
 
 async function run () {
     try {
@@ -28,22 +43,31 @@ async function run () {
         console.log("database connected");
 
         const itemCollection = client.db("bookMania").collection("items");
-        const userItemsCollection = client.db("bookMania").collection("userItems");
+        // const userItemsCollection = client.db("bookMania").collection("userItems");
+        // console.log(itemCollection);
 
-        app.post("/addItems", async (req, res) => {
-            const item = req.body;
-
+        app.get("/items", async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
         });
 
-        app.get('/manage-inventory', async (req, res) => {
-            const items = await itemCollection.find({}).toArray();
-            req.send(items);
-        });
+        // app.post("/add-items", async (req, res) => {
+        //     const item = req.body;
+        //     const result = await itemCollection.insertOne(item);
+        //     res.send({ success: 'product added' });
+        // });
 
-        app.get('/my-items', async (req, res) => {
-            const userItems = await userItemsCollection.find({ email }).toArray();
-            req.send(userItems);
-        });
+        // app.get('/manage-inventory', async (req, res) => {
+        //     const items = await itemCollection.find({}).toArray();
+        //     req.send(items);
+        // });
+
+        // app.get('/my-items', async (req, res) => {
+        //     const userItems = await userItemsCollection.find({ email }).toArray();
+        //     req.send(userItems);
+        // });
     }
     finally {
 
